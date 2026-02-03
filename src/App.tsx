d4c704448cda91bc68d0f8faf0cb0beaf7ef42f6
Nethan Nagendran <nethan.nagendran@gmail.com>
2026-02-03 15:58:24 -0500

    created skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [calGoal, setcalGoal] = useState(0)
+  const [proteinGoal, setproteinGoal] = useState(0)
 
   return (
-    <div>
-      <p>Everything works!</p>
+    <div className='main-container'>
+      <div className='fitness-info'>
+        <h1>Fitness Info</h1>
+        <p>Cal Goal: 0</p>
+        <p>Protein Goal: 0</p>
+        <p>Carbs Goal: 0</p>
+        <button>Edit Goals</button>
+      </div>
+
+
+      <div className='fitness-meals'>
+        <h1>Fitness Meals</h1>
+      </div>
+
+      
+      <div className='add-meals'>
+        <button>Add meal</button>
+      </div>
     </div>
   )
 }
